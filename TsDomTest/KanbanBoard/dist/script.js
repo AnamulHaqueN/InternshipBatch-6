d9163a6b65654b7143e9e154770e2b5d53e14c86
AnamulHaqueN <rafaielanam@gmail.com>
2025-09-29 19:29:05 +0600

    todays end task

--- TsDomTest/KanbanBoard/dist/script.js
+++ TsDomTest/KanbanBoard/dist/script.js
@@ -3,24 +3,32 @@ const title = document.querySelector('#title');
 const description = document.querySelector('#description');
 const assignedUser = document.querySelector('#assigned-user');
 const div = document.querySelector('#task');
-const ul = document.createElement('ul');
-let att = document.createAttribute("class");
-att.value = "display";
-ul.setAttributeNode(att);
+let taskArr = [];
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    const ul = document.createElement('ul');
+    let att = document.createAttribute("class");
+    att.value = "display";
+    ul.setAttributeNode(att);
     let TaskData = {
         title: title.value,
         description: description.value,
         assignedUser: assignedUser.value
     };
+    taskArr.push(TaskData);
+    localStorage.setItem('taskArr', JSON.stringify(taskArr));
+    alert('Form data saved to localStorage!');
+    const getTask = localStorage.getItem("taskArr");
     ul.innerHTML = `
        <li><b>Title</b>: ${TaskData.title}</li>
        <li>Description: ${TaskData.description}</li>
        <li>Assigned User: ${TaskData.assignedUser}</li>
+       <button id="start-task">start task</button>
     `;
     div === null || div === void 0 ? void 0 : div.appendChild(ul);
-    console.log(TaskData);
+    //console.log(TaskData);
 });
+const getTask = localStorage.getItem("taskArr");
+console.log(getTask);
 export {};
 //# sourceMappingURL=script.js.map
--- TsDomTest/KanbanBoard/src/script.ts
+++ TsDomTest/KanbanBoard/src/script.ts
@@ -8,29 +8,41 @@ const description = document.querySelector('#description') as HTMLInputElement;
 const assignedUser = document.querySelector('#assigned-user') as HTMLInputElement;
 
 const div = document.querySelector('#task');
-const ul = document.createElement('ul');
-let att = document.createAttribute("class");
-att.value = "display";
-ul.setAttributeNode(att);
+
+let taskArr = [];
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
-  
+    const ul = document.createElement('ul');
+    let att = document.createAttribute("class");
+    att.value = "display";
+    ul.setAttributeNode(att);
     let TaskData = {
         title: title.value,
         description: description.value,
         assignedUser: assignedUser.value
     }
+    
+    taskArr.push(TaskData);
+    localStorage.setItem('taskArr', JSON.stringify(taskArr));
+    alert('Form data saved to localStorage!');
+    
+    const getTask = localStorage.getItem("taskArr");
 
     ul.innerHTML = `
        <li><b>Title</b>: ${TaskData.title}</li>
        <li>Description: ${TaskData.description}</li>
        <li>Assigned User: ${TaskData.assignedUser}</li>
+       <button id="start-task">start task</button>
     `
     div?.appendChild(ul);
-    console.log(TaskData);
+    //console.log(TaskData);
     
 });
+const getTask = localStorage.getItem("taskArr");
+console.log(getTask);
+
+
 
 
 
