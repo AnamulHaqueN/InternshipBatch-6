bcca613385e21501286583c2c1162bef6194d3b1
AnamulHaqueN <rafaielanam@gmail.com>
2025-09-30 11:27:50 +0600

    basic style for todo is ready

--- TsDomTest/KanbanBoard/src/script.ts
+++ TsDomTest/KanbanBoard/src/script.ts
@@ -1,46 +1,69 @@
 
+type FormData = {
+  title: string,
+  description: string,
+  assignUser: string
+}
+
+const showFormBtn = document.getElementById('show-form-btn') as HTMLButtonElement;
 const form = document.querySelector('.task-form') as HTMLFormElement;
 
-const title = document.querySelector('#title') as HTMLInputElement;
+const div = document.querySelector('#task') as HTMLDivElement;
 
-const description = document.querySelector('#description') as HTMLInputElement;
+showFormBtn.addEventListener('click', () => {
+  if(form.style.display === "none") {
+    form.style.display = "block";
+    showFormBtn.style.display = "none";
+  }
+});
 
-const assignedUser = document.querySelector('#assigned-user') as HTMLInputElement;
+(window as any).deleteTask = (index: number) => {
+  const tasks: FormData[] = JSON.parse(localStorage.getItem('formData') || '[]');
+  tasks.splice(index, 1);
+  localStorage.setItem('formData', JSON.stringify(tasks));
+  displayData();
+};
 
-const div = document.querySelector('#task');
-
-let taskArr = [];
+function displayData() {
+  const stroreData: FormData[] = JSON.parse(localStorage.getItem('formData') || '[]');
+  if(stroreData.length === 0) {
+    div.innerHTML = `<p> No data is found </p>`;
+    return;
+  }
+  div.innerHTML = stroreData.map((item, index) => 
+    `
+       <p><b>Title: </b> ${item.title}</p>
+       <p>Description: ${item.description}</p>
+       <p>Assigned User: ${item.assignUser}</p>
+       <button onclick="deleteTask(${index})">Delete</button>
+       <hr>
+    `
+  ) 
+  .join("");  
+}
+displayData();
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
-    const ul = document.createElement('ul');
-    let att = document.createAttribute("class");
-    att.value = "display";
-    ul.setAttributeNode(att);
-    let TaskData = {
-        title: title.value,
-        description: description.value,
-        assignedUser: assignedUser.value
-    }
-    
-    taskArr.push(TaskData);
-    localStorage.setItem('taskArr', JSON.stringify(taskArr));
-    alert('Form data saved to localStorage!');
-    
-    const getTask = localStorage.getItem("taskArr");
+  const title = (document.querySelector('#title') as HTMLInputElement).value;
+  const description = (document.querySelector('#description') as HTMLInputElement).value;
+  const assignUser = (document.querySelector('#assigned-user') as HTMLInputElement).value;
+  
+  const data: FormData = {
+    title: title,
+    description: description,
+    assignUser: assignUser
+  }
 
-    ul.innerHTML = `
-       <li><b>Title</b>: ${TaskData.title}</li>
-       <li>Description: ${TaskData.description}</li>
-       <li>Assigned User: ${TaskData.assignedUser}</li>
-       <button id="start-task">start task</button>
-    `
-    div?.appendChild(ul);
-    //console.log(TaskData);
-    
+  const getTask = JSON.parse(localStorage.getItem('formData') || '[]');
+  getTask.push(data);
+
+  localStorage.setItem("formData", JSON.stringify(getTask));
+  displayData();
+  form.style.display = "none";
+  showFormBtn.style.display = "block";
+  
 });
-const getTask = localStorage.getItem("taskArr");
-console.log(getTask);
 
 
 
